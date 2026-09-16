a03cb11e832194ff7fdbec808e0fbf083c23b33f
Prince-ABES <prk988110@gmail.com>
2026-09-16 22:24:56 +0530

    expressSetup

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,18 @@
 import dotenv from "dotenv";
-
+import express from "express";
 dotenv.config({
     path:"./.env",
 });
+
+const app =express();
+const port=process.env.PORT || 3000;
+
+app.get("/",(req,res)=>{
+    res.send("Hello world");
+})
+
+app.listen(port,()=>{
+    console.log(`Server is listening on port http://localhost:${port}`);
+})
 
 console.log("welcom to MandiMantra");
