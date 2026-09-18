2777de221365c2b426c995401ba35edd91dcc284
Prince-ABES <prk988110@gmail.com>
2026-09-18 22:59:24 +0530

    added ReactRouter

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div><h1>Welcome to Mandi mantra</h1></div>
-  )
+    <Routes>
+      <Route path="/" element={<Navigate to="/Home" replace />} />
+      <Route path="/Home" element={<Home />} />
+    </Routes>
+  );
 }
 
 export default App;
